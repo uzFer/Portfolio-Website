c3f7d280b85ec8995ff284f3226a538ec6669986
uzFer <109243682+uzFer@users.noreply.github.com>
2022-07-13 15:35:49 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+const navLogo = document.querySelector('#navbar_logo');
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
